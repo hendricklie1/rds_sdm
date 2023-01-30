7cfbd2df47b73239675507de408d24a946ac9981
hendrickbymart <75781521+hendrickbymart@users.noreply.github.com>
2023-01-30 20:37:51 +0700

    Update tambah.php
    Perbaiki tampilan

--- application/views/pegawai/tambah.php
+++ application/views/pegawai/tambah.php
@@ -27,16 +27,16 @@
 							<input type="text" class="form-control" name="nama" required>
 						</div>
 					</div>
-                    <div class="row" style="margin-top:10px;">
-				<div class="col-md-2">
-					<label class="text-uppercase">Gender</label>
-				</div>
-				<div class="col-md-8">
-					<label class="radio-inline"><input type="radio" name="gender" value="pria">Pria</label>
-					<label class="radio-inline"><input type="radio" name="gender" value="wanita">Wanita</label>
-				</div>
-			</div>
-            <div class="col-md-12" style="margin-top:10px;">
+                    <div class="col-md-12" style="margin-top:10px;">
+						<div class="col-md-2">
+							<label class="text-uppercase">Gender</label>
+						</div>
+						<div class="col-md-8">
+							<label class="radio-inline"><input type="radio" name="gender" value="pria">Pria</label>
+							<label class="radio-inline"><input type="radio" name="gender" value="wanita">Wanita</label>
+						</div>
+					</div>
+            		<div class="col-md-12" style="margin-top:10px;">
 						<div class="col-md-2">
 							<label class="text-uppercase">Tempat Lahir</label>
 						</div>
@@ -44,15 +44,15 @@
 							<input type="text" class="form-control" name="ttl" required>
 						</div>
 					</div>
-            <div class="row" style="margin-top:15px;">
-				<div class="col-md-2">
-					<label class="text-uppercase">Tanggal Lahir</label>
-				</div>
-				<div class="col-md-3">
-					<input type="text" class="form-control datepicker" name="tgllahir" value="" autocomplete="off">
-				</div>
-			</div>
-            <div class="col-md-12" style="margin-top:10px;">
+					<div class="col-md-12" style="margin-top:15px;">
+						<div class="col-md-2">
+							<label class="text-uppercase">Tanggal Lahir</label>
+						</div>
+						<div class="col-md-3">
+							<input type="text" class="form-control datepicker" name="tgllahir" value="" autocomplete="off">
+						</div>
+					</div>
+            		<div class="col-md-12" style="margin-top:10px;">
 						<div class="col-md-2">
 							<label class="text-uppercase">Agama</label>
 						</div>
@@ -60,16 +60,16 @@
 							<input type="text" class="form-control" name="agama" required>
 						</div>
 					</div>
-                    <div class="row" style="margin-top:10px;">
-				<div class="col-md-2">
-					<label class="text-uppercase">Status</label>
-				</div>
-				<div class="col-md-8">
-					<label class="radio-inline"><input type="radio" name="status" value="aktif">Aktif</label>
-					<label class="radio-inline"><input type="radio" name="status" value="tidak aktif">Tidak Aktif</label>
-				</div>
-			</div>
-            <div class="col-md-12" style="margin-top:10px;">
+                    <div class="col-md-12" style="margin-top:10px;">
+						<div class="col-md-2">
+							<label class="text-uppercase">Status</label>
+						</div>
+						<div class="col-md-8">
+							<label class="radio-inline"><input type="radio" name="status" value="aktif">Aktif</label>
+							<label class="radio-inline"><input type="radio" name="status" value="tidak aktif">Tidak Aktif</label>
+						</div>
+					</div>
+            		<div class="col-md-12" style="margin-top:10px;">
 						<div class="col-md-2">
 							<label class="text-uppercase">Pendidikan</label>
 						</div>
@@ -77,15 +77,15 @@
 							<input type="text" class="form-control" name="pendidikan" required>
 						</div>
 					</div>
-                    <div class="row" style="margin-top:15px;">
-				<div class="col-md-2">
-					<label class="text-uppercase">Alamat</label>
-				</div>
-				<div class="col-md-8">
-					<textarea class="form-control" name="alamat" style="height:100px;resize:none;"></textarea>
-				</div>
-			</div>
-            <div class="col-md-12" style="margin-top:10px;">
+                    <div class="col-md-12" style="margin-top:15px;">
+						<div class="col-md-2">
+							<label class="text-uppercase">Alamat</label>
+						</div>
+						<div class="col-md-8">
+							<textarea class="form-control" name="alamat" style="height:100px;resize:none;"></textarea>
+						</div>
+					</div>
+            		<div class="col-md-12" style="margin-top:10px;">
 						<div class="text-center">
 							<button class="btn btn-primary" type="submit"><i class="fa fa-save"></i> Simpan</button>
 						</div>	
